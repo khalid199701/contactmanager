a4289c357d43708391fe34561ca534c114a975c3
khalid199701 <ayon201401@gmail.com>
2019-10-06 01:52:34 +0600

    Updates

---
+++ precache-manifest.b381cacd823c57103515241ee2b14a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c11ea79102fcb7cf006242c6b955903",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "9b9f69a4c62fdf1b74e0",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "51e9963c1fa9f309c83c",
+    "url": "/contactmanager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9b9f69a4c62fdf1b74e0",
+    "url": "/contactmanager/static/js/2.5bbf4dfd.chunk.js"
+  },
+  {
+    "revision": "51e9963c1fa9f309c83c",
+    "url": "/contactmanager/static/js/main.d7313f92.chunk.js"
+  },
+  {
+    "revision": "485c57d5d1665b41ffe4",
+    "url": "/contactmanager/static/js/runtime-main.529d79c8.js"
+  }
+]);
